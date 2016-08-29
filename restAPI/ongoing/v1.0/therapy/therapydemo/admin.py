22865f229c172246dd4b1bbb086d3738adb4db6c
pritamprasad0 <pritam.prasd@gmail.com>
2016-08-29 17:43:30 +0530

    Updated new Tables in DB

--- restAPI/ongoing/v1.0/therapy/therapydemo/admin.py
+++ restAPI/ongoing/v1.0/therapy/therapydemo/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from .models import Folder
+from .models import *
 
 admin.site.register(Folder)
+admin.site.register(Video)
+admin.site.register(Video_folder_mapping)
 
--- restAPI/ongoing/v1.0/therapy/therapydemo/models.py
+++ restAPI/ongoing/v1.0/therapy/therapydemo/models.py
@@ -1,12 +1,30 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.utils import timezone
 
 class Folder(models.Model):
-    folderid = models.IntegerField()
-    foldername = models.CharField(max_length=20)
-    parentid = models.IntegerField()
+    folder_id = models.IntegerField(primary_key=True,blank=False)
+    folder_name = models.CharField(max_length=20, blank=False)
+    parent_id = models.IntegerField()
 
     def __str__(self):
-        return self.foldername
+        return self.folder_name
+
+
+
+class Video(models.Model):
+    video_id = models.IntegerField(blank=False,primary_key=True)
+    video_name = models.CharField(max_length=255,blank=False)
+    video_path = models.CharField(max_length=255,blank=False)
+    video_date_modified = models.DateTimeField(default=timezone.now())
+
+    def __str__(self):
+        return self.video_name
+
+
+class Video_folder_mapping(models.Model):
+    video_id = models.ForeignKey(Video)
+    folder_id = models.ForeignKey(Folder)
+
 
